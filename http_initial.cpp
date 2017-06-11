a68089ac942a6abf5d3346cbbf8e5cecd23a2a18
Daniel Zerlett <daniel@zerlett.eu>
2017-06-11 21:31:03 +0200

    SPIFFS

--- http_initial.cpp
+++ http_initial.cpp
@@ -1,29 +1,25 @@
 #include <Arduino.h>
+#include "FS.h"
 #include "http.h"
 #include "config.h"
-
-const char http_initial_html[] PROGMEM = R"=====(
-  <h1>ESP8266</h1>
-  <h2>Initial configuration</h2>
-  <form action="/" method="POST">
-    SSID: <input name="ssid"> (max. 32 characters)<br>
-    Password: <input name="password" type="password"> (max. 32 characters)<br>
-    <input type="submit" value="save">
-  </form>
-)=====";
-
-const char http_initial_reboot_html[] PROGMEM = R"=====(
-  <h1>ESP8266</h1>
-  <h2>Initial configuration was applied</h2>
-  Please wait for the device to be rebooted.
-)=====";
 
 void http_initial_setup() {
   Serial.println("Setting up HTTP server for initial config");
   http_server.on("/", HTTP_GET, []() {
     Serial.println("GET / 200");
-    http_server.send_P (200, "text/html", http_initial_html); 
+    SPIFFS.begin();
+
+    File f = SPIFFS.open("/initial_index.html", "r");
+    if (!f) {
+      Serial.println("file open failed");
+    }
+    
+    String s = f.readString();
+    Serial.println(s);
+    
+    http_server.send_P (200, "text/html", s.c_str()); 
   });
+  /*
   http_server.on("/", HTTP_POST, []() {
     Serial.println("POST /init 200");       
     config_set_wifi_ssid(http_server.arg("ssid").c_str());
@@ -31,7 +27,8 @@ void http_initial_setup() {
     config_write();
     http_server.send_P (200, "text/html", http_initial_reboot_html); 
     ESP.restart();
-  });  
+  });
+  */  
 
   http_setup();
 }
