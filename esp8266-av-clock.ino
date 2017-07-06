f936a8f5423d8f006cac46212f213d09c7917d86
Daniel Zerlett <daniel@zerlett.eu>
2017-07-07 01:58:37 +0200

    initial commit

---
+++ esp8266-av-clock.ino
@@ -0,0 +1,42 @@
+#include "config.h"
+#include "wifi.h"
+#include "http.h"
+#include "http_initial.h"
+#include "http_regular.h"
+#include "app_main.h"
+
+void setup(void){
+
+  pinMode(16, INPUT_PULLUP);  
+  
+  Serial.begin(115200);
+  Serial.println(F("\nStarted"));
+
+  config_setup();
+
+  // if GPIO 16 is low, wipe EEPROM
+  boolean config_reset = (digitalRead(16) == 0);
+  if (config_reset) {
+    config_wipe();
+  }
+
+  // try to read config
+  boolean config_ok = config_read();
+  if (!config_ok) {
+    Serial.println(F("No valid config found"));
+    wifi_create_ap();     
+    http_initial_setup();
+  } else {
+    wifi_connect();  
+    http_regular_setup();
+    app_setup();
+  } 
+}
+
+void loop(void){ 
+  http_handle();
+  app_loop();
+}
+
+
+
