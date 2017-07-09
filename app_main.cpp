4b445c9ae1e9de5260928efb050529500850a546
Daniel Zerlett <daniel@zerlett.eu>
2017-07-09 23:10:01 +0200

    hooks for wifi connecting

--- app_main.cpp
+++ app_main.cpp
@@ -3,6 +3,12 @@
 void app_setup() {  
 }
 
+void app_connecting() {  
+}
+
+void app_connected() {  
+}
+
 void app_loop() {
 }
 
--- app_main.h
+++ app_main.h
@@ -1,2 +1,4 @@
 void app_setup();
+void app_connecing();
+void app_connected();
 void app_loop();
